ab3519bbbef58b78e785d6ffb50ed72f21630ba0
tomhuhges <merzbear@gmail.com>
2017-03-09 15:03:38 +0000

    add error if argument is not json

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const groupArray = require('group-array')
 const getTimeBlocks = require('./getTimeBlocks')
 const format = require('./format')
 
+if (!~process.argv[2].indexOf('.json')) {
+  throw new Error("argument must be a json file")
+}
+
 const filename = `${__dirname}/${process.argv[2]}`
 const newFilename = process.argv[2].replace('.json', '_(copy).json')
 
